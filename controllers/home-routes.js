5658a9293c07315d98f4a63bc90c5e1b489f03a4
BaharMahmud <bahar.a.mahmud@gmail.com>
2024-03-04 20:59:14 -0500

    READM addedd and changes mad ein contoroller folder

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,41 +2,9 @@ const router = require('express').Router();
 const { greenhouse, plant } = require('../models');
 const withAuth = require('../utils/auth');
 
-// GET all plants in the greenhouse
 
-router.get('/greenhouse',withAuth, async (req, res) => {
-  try {
-    const dbPlantData = await plant.findAll({
-      include: [
-        {
-          model: plant,
-          attributes: ['id',
-          'title',
-          'artist',
-          'exhibition_date',
-          'filename',
-          'description',
-         ],
-        },
-      ],
-    });
-
-    const plants = dbPlantData.map((plant) =>
-      plant.get({ plain: true })
-    );
-
-    res.render('homepage', {
-      plants,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// GET all plants for PlantShelf
-router.get('/plant',withAuth, async (req, res) => {
+// GET all plants for greenhouse
+router.get('/gallery',withAuth, async (req, res) => {
   try {
     const dbPlantData = await plant.findAll({
       include: [
