e4df378059f5d6cf9a4dc97beec78788ffd2d530
Gabrielle 🌿 <gabrielle@gabrieleslaptop.mynetworksettings.com>
2024-03-06 18:49:38 -0500

    added comment in seeds/plantshelf.js

--- seeds/plantshelf.js
+++ seeds/plantshelf.js
@@ -1,6 +1,8 @@
 const sequelize = require ('../config/connection');
+///import greenhouse and user data?
 const { Plant } = require('../models');
 
+//import userdata.json?
 const plantdata = require('./plantdata.json')
 
 const seedPlant = async () => {
