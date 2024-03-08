abf58f3271cb771211960d6ff8d30a8dcae8f7f9
BaharMahmud <bahar.a.mahmud@gmail.com>
2024-03-07 21:13:19 -0500

    additions

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+//const withAuth = require('../utils/auth');
 
+
+
+// Login
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Plant } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
+    const plantData = await Plant.findAll({    
       order: [['name', 'ASC']],
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    const plants = plantData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
-      users,
+    res.render('greenhouse', {
+      plants,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
